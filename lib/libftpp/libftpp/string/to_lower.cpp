a6020090f4f8488b54468738741d37df9f7dc72c
David Poetsch <poetschdavid@gmail.com>
2025-10-08 15:25:20 +0200

    style: Change formatting
    Files where formatted wrong because i used and old clangd version.

--- lib/libftpp/libftpp/string/to_lower.cpp
+++ lib/libftpp/libftpp/string/to_lower.cpp
@@ -3,21 +3,24 @@
 #include <cctype>
 #include <string>
 
-static char to_lowercase(unsigned char c) {
-  return static_cast<char>(std::tolower(c));
+static char to_lowercase(unsigned char c)
+{
+	return static_cast<char>(std::tolower(c));
 }
 
 namespace ft {
 
-std::string &to_lower(std::string &str) {
-  std::transform(str.begin(), str.end(), str.begin(), to_lowercase);
-  return str;
+std::string& to_lower(std::string& str)
+{
+	std::transform(str.begin(), str.end(), str.begin(), to_lowercase);
+	return str;
 }
 
-std::string to_lower(const std::string &str) {
-  std::string newStr = str;
-  std::transform(str.begin(), str.end(), newStr.begin(), to_lowercase);
-  return newStr;
+std::string to_lower(const std::string& str)
+{
+	std::string newStr = str;
+	std::transform(str.begin(), str.end(), newStr.begin(), to_lowercase);
+	return newStr;
 }
 
 } // namespace ft
--- lib/libftpp/libftpp/string/to_upper.cpp
+++ lib/libftpp/libftpp/string/to_upper.cpp
@@ -3,21 +3,24 @@
 #include <cctype>
 #include <string>
 
-static char to_uppercase(unsigned char c) {
-  return static_cast<char>(std::toupper(c));
+static char to_uppercase(unsigned char c)
+{
+	return static_cast<char>(std::toupper(c));
 }
 
 namespace ft {
 
-std::string &to_upper(std::string &str) {
-  std::transform(str.begin(), str.end(), str.begin(), to_uppercase);
-  return str;
+std::string& to_upper(std::string& str)
+{
+	std::transform(str.begin(), str.end(), str.begin(), to_uppercase);
+	return str;
 }
 
-std::string to_upper(const std::string &str) {
-  std::string newStr = str;
-  std::transform(str.begin(), str.end(), newStr.begin(), to_uppercase);
-  return newStr;
+std::string to_upper(const std::string& str)
+{
+	std::string newStr = str;
+	std::transform(str.begin(), str.end(), newStr.begin(), to_uppercase);
+	return newStr;
 }
 
 } // namespace ft
