1bc0f8adbd19c69347bd8c9eacc1abd1cfa1a1bd
David Poetsch <poetschdavid@gmail.com>
2025-10-08 15:36:50 +0200

    refactor: Change functions to use C functions

--- lib/libftpp/libftpp/string/to_lower.cpp
+++ lib/libftpp/libftpp/string/to_lower.cpp
@@ -1,26 +1,20 @@
 #include "libftpp/string.hpp"
 #include <algorithm>
-#include <cctype>
+#include <ctype.h>
 #include <string>
-
-static char to_lowercase(unsigned char c)
-{
-	return static_cast<char>(std::tolower(c));
-}
 
 namespace ft {
 
 std::string& to_lower(std::string& str)
 {
-	std::transform(str.begin(), str.end(), str.begin(), to_lowercase);
+	std::transform(str.begin(), str.end(), str.begin(), ::tolower);
 	return str;
 }
 
 std::string to_lower(const std::string& str)
 {
-	std::string newStr = str;
-	std::transform(str.begin(), str.end(), newStr.begin(), to_lowercase);
-	return newStr;
+	std::string newStr(str);
+	return to_lower(newStr);
 }
 
 } // namespace ft
--- lib/libftpp/libftpp/string/to_upper.cpp
+++ lib/libftpp/libftpp/string/to_upper.cpp
@@ -1,26 +1,20 @@
 #include "libftpp/string.hpp"
 #include <algorithm>
-#include <cctype>
+#include <ctype.h>
 #include <string>
-
-static char to_uppercase(unsigned char c)
-{
-	return static_cast<char>(std::toupper(c));
-}
 
 namespace ft {
 
 std::string& to_upper(std::string& str)
 {
-	std::transform(str.begin(), str.end(), str.begin(), to_uppercase);
+	std::transform(str.begin(), str.end(), str.begin(), ::toupper);
 	return str;
 }
 
 std::string to_upper(const std::string& str)
 {
-	std::string newStr = str;
-	std::transform(str.begin(), str.end(), newStr.begin(), to_uppercase);
-	return newStr;
+	std::string newStr(str);
+	return to_upper(newStr);
 }
 
 } // namespace ft
