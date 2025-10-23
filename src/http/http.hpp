236053b0665cdecb593d90b6640dfdca276c22a5
David Poetsch <poetschdavid@gmail.com>
2025-10-23 12:56:09 +0200

    refactor: Refactor remove unused lints

--- src/http/http.hpp
+++ src/http/http.hpp
@@ -2,8 +2,6 @@
 #ifndef HTTP_HPP
 #define HTTP_HPP
 
-// NOLINTBEGIN(readability-identifier-length,
-// clang-diagnostic-unused-const-variable)
 namespace http {
 int isSchemeChar(int chr);
 int isAuthChar(int chr);
@@ -19,7 +17,5 @@ int isDigit19(int chr);
 int isDigit04(int chr);
 int isDigit05(int chr);
 }
-// NOLINTEND(readability-identifier-length,
-// clang-diagnostic-unused-const-variable)
 
 #endif // HTTP_HPP
