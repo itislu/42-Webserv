58981be1093b4e19e27453b94891f6908613a479
David Poetsch <poetschdavid@gmail.com>
2025-11-05 17:46:11 +0100

    feat: Add Logger

---
+++ src/utils/logger/Logger.cpp
@@ -0,0 +1,104 @@
+#include "Logger.hpp"
+
+#include "libftpp/memory.hpp"
+#include "libftpp/utility.hpp"
+
+#include <ctime>
+#include <exception>
+#include <fstream>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <map>
+#include <string>
+
+/* ************************************************************************** */
+// PUBLIC
+
+Logger& Logger::getInstance(const char* filename)
+{
+  const static Logger singelton; // necessary for cleanup
+
+  try {
+    return *_instances().at(filename);
+  } catch (const std::exception& e) {
+    const ft::shared_ptr<Logger> LoggerPtr(new Logger(filename));
+    _instances()[filename] = LoggerPtr;
+    return *LoggerPtr;
+  }
+}
+
+std::ostream& Logger::info()
+{
+  return _log(INFO);
+}
+std::ostream& Logger::warning()
+{
+  return _log(WARNING);
+}
+std::ostream& Logger::error()
+{
+  return _log(ERROR);
+}
+
+/* ***************************************************************************/
+// PRIVATE
+Logger::Logger() {}
+
+Logger::Logger(const char* filename)
+{
+  _file.open(filename, std::ios::out | std::ios::trunc);
+  _file << std::unitbuf; // enables automatic flush
+  if (!_file) {
+    std::cerr << "Failed to open log file: " << filename << '\n';
+  }
+}
+
+Logger::~Logger()
+{
+  if (_file.is_open()) {
+    _file.close();
+  }
+}
+
+// NOLINTBEGIN(performance-avoid-endl)
+std::ostream& Logger::_log(LogLevel level)
+{
+  if (!_file.is_open()) {
+    return _file;
+  }
+  std::string levelStr;
+  switch (level) {
+    case INFO:
+      levelStr = "INFO";
+      break;
+    case WARNING:
+      levelStr = "WARNING";
+      break;
+    case ERROR:
+      levelStr = "ERROR";
+      break;
+  }
+  _file << "[" << _currentTime() << "] [";
+  _file << std::setw(_widthLevelStr) << levelStr << "] ";
+  return _file;
+}
+// NOLINTEND(performance-avoid-endl)
+
+Logger::InstanceMap& Logger::_instances()
+{
+  static InstanceMap instances;
+  return instances;
+}
+
+// NOLINTBEGIN(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+std::string Logger::_currentTime()
+{
+  std::time_t const now = std::time(FT_NULLPTR);
+  const int bufSize = 32;
+  char buff[bufSize];
+  (void)std::strftime(
+    buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", std::localtime(&now));
+  return buff;
+}
+// NOLINTEND(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
---
+++ src/utils/logger/Logger.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#ifndef LOGGER_HPP
+#define LOGGER_HPP
+
+#include "libftpp/memory.hpp"
+
+#include <fstream>
+#include <map>
+#include <ostream>
+#include <string>
+
+namespace logFiles {
+const char* const general = "./log/general.log";
+const char* const http = "./log/http.log";
+}
+
+/* ************************************************************************** */
+class Logger
+{
+public:
+  enum LogLevel
+  {
+    INFO,
+    WARNING,
+    ERROR
+  };
+
+  static Logger& getInstance(const char* filename);
+
+  std::ostream& info();
+  std::ostream& warning();
+  std::ostream& error();
+
+  ~Logger();
+
+private:
+  typedef std::map<const char*, ft::shared_ptr<Logger> > InstanceMap;
+
+  Logger();
+  explicit Logger(const char* filename);
+  Logger(const Logger& other);
+  Logger& operator=(const Logger& other);
+
+  std::ostream& _log(LogLevel level);
+  static InstanceMap& _instances();
+  static std::string _currentTime();
+
+  static const int _widthLevelStr = 7;
+
+  std::ofstream _file;
+};
+
+#endif
