1e4163fa3375398829813399a3d4bf163c775c5d
Lukas Stefanek <lstefane@student.42vienna.com>
2025-11-27 17:35:43 +0100

    feat: Add autoindex generation

--- src/http/states/prepareResponse/HandleGet.cpp
+++ src/http/states/prepareResponse/HandleGet.cpp
@@ -1,4 +1,6 @@
 #include "HandleGet.hpp"
+#include "config/FileUtils.hpp"
+#include "libftpp/utility.hpp"
 
 #include <client/Client.hpp>
 #include <http/Headers.hpp>
@@ -9,7 +11,10 @@
 #include <utils/logger/Logger.hpp>
 #include <utils/state/IState.hpp>
 
+#include <cstring>
+#include <dirent.h>
 #include <fstream>
+#include <iostream>
 #include <sstream>
 #include <string>
 #include <sys/stat.h>
@@ -89,4 +94,47 @@ void HandleGet::_openFile()
 
   std::ifstream& body = _client->getResponse().getBody();
   body.open(file.c_str());
+}
+
+/* Generates a HTML for the autoindex setting in a location if no index.html is
+  present. If the entry in the folder is directory it generates a clickable
+  link. If the entry is a regular file it just lists the name.
+
+  TODO: change string conncatination to .append() - for faster performance
+*/
+std::string HandleGet::_generateAutoindex(const std::string& path)
+{
+  DIR* const dir = opendir(path.c_str());
+  if (dir == FT_NULLPTR) {
+    return "<html><body><h1>403 Forbidden</h1></body></html>\n";
+  }
+
+  std::string html;
+  html += "<html><head><title>Index of " + path + "</title></head><body>\n";
+  html += "<h1>Index of " + path + "</h1>\n<ul>\n";
+
+  struct dirent* entry = 0;
+  while ((entry = readdir(dir)) != 0) {
+    const char* const name = static_cast<char*>(entry->d_name);
+    if (std::strcmp(name, ".") == 0 || std::strcmp(name, "..") == 0) {
+      continue;
+    }
+
+    const std::string fullPath = path + "/" + name;
+    if (config::fileutils::isDirectory(fullPath)) {
+      html += "<li><a href=\"";
+      html += name;
+      html += "/\">";
+      html += name;
+      html += "/</a></li>\n";
+    } else {
+      html += "<li>";
+      html += name;
+      html += "</li>\n";
+    }
+  }
+
+  html += "</ul></body></html>\n";
+  closedir(dir);
+  return html;
 }
