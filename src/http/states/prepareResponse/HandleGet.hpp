9165055ca20244a3c0ffcb39f1971a15aa85574e
Lukas Stefanek <lstefane@student.42vienna.com>
2025-12-15 15:28:09 +0100

    fix: Fix formatting for linter

--- src/http/states/prepareResponse/HandleGet.hpp
+++ src/http/states/prepareResponse/HandleGet.hpp
@@ -23,7 +23,9 @@ private:
   void _setNextState();
   void _handleAutoIndex();
   void _handleStaticFile();
-  void _generateAutoindex(const std::string& dirPath, const std::string& path, SmartBuffer& html);
+  void _generateAutoindex(const std::string& dirPath,
+                          const std::string& path,
+                          SmartBuffer& html);
 
   static Logger& _log;
   PrepareResponse* _prepareResponse;
